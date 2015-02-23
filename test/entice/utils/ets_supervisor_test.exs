4b743c4fe22eb4934221e69da2f50800347bcf32
ephe-meral <j.appel.online@gmail.com>
2015-02-23 15:31:36 +0100

    adds check for termination method call

--- test/entice/utils/ets_supervisor_test.exs
+++ test/entice/utils/ets_supervisor_test.exs
@@ -9,10 +9,20 @@ defmodule Entice.Utils.ETSSupervisorTest do
     def start_link(_id, state),
     do: GenServer.start_link(__MODULE__, state)
 
-    def init(state), do: {:ok, state}
+    def init(state) do
+      Process.flag(:trap_exit, true)
+      send state, :received_init
+      {:ok, state}
+    end
 
-    def handle_call(:test_call, _from, state),
-    do: {:reply, state, state}
+    def handle_call(:test_call, _from, state) do
+      send state, :received_call
+      {:reply, state, state}
+    end
+
+    def terminate(_reason, state) do
+      send state, :received_terminate
+    end
   end
 
 
@@ -23,21 +33,29 @@ defmodule Entice.Utils.ETSSupervisorTest do
 
 
   test "creation" do
-    assert {:ok, _pid} = ETSSupervisor.start(__MODULE__, "some_id1", [:some_state])
-    assert {:error, _reason, _other_pid} = ETSSupervisor.start(__MODULE__, "some_id1", :some_state)
+    assert {:ok, _pid} = ETSSupervisor.start(__MODULE__, "some_id1", [self])
+    assert_receive :received_init
+    assert {:error, _reason, _other_pid} = ETSSupervisor.start(__MODULE__, "some_id1", [self])
   end
 
+
   test "deletion" do
-    {:ok, pid} = ETSSupervisor.start(__MODULE__, "some_id2", [:some_state])
+    {:ok, pid} = ETSSupervisor.start(__MODULE__, "some_id2", [self])
+    assert_receive :received_init
     assert Process.alive?(pid) == true
+
     assert ETSSupervisor.terminate(__MODULE__, "some_id2") == :ok
     assert ETSSupervisor.terminate(__MODULE__, "some_id2") == :error
     assert ETSSupervisor.terminate(__MODULE__, "no_id") == :error
+
     assert Process.alive?(pid) == false
+    assert_receive :received_terminate
   end
 
+
   test "retrieval" do
-    ETSSupervisor.start(__MODULE__, "some_id3", [:some_state])
+    ETSSupervisor.start(__MODULE__, "some_id3", [self])
+    assert_receive :received_init
     assert {:ok, _pid} = ETSSupervisor.lookup(__MODULE__, "some_id3")
     assert {:error, _reason} = ETSSupervisor.lookup(__MODULE__, "no_id")
   end
